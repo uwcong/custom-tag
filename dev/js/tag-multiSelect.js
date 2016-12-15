32964e860ccf700aca0df11a7a457b8ef4349281
Cc <uwcong@qq.com>
2016-12-15 15:03:19 +0800

    multiSelector and submit

--- dev/js/tag-multiSelect.js
+++ dev/js/tag-multiSelect.js
@@ -1,8 +1,11 @@
+/**
+ * 多选下拉组件（配合提交使用）
+ * @author Cc
+ * @date 2016/12/14
+ */
 var Selector = {
   /**
-   * 渲染选项数据
-   * @author Cc
-   * @date 2016/12/14
+   * private function 渲染选项数据
    * @param {object} dom
    * @param {object} obj
    * @param {string} obj.id
@@ -10,27 +13,84 @@ var Selector = {
    * @param {string} obj.name
    * @param {array}  obj.data
    */
-  renderData: function(dom, obj) {
-    var temp = "";
-    for(var i=0, len=obj['data'].length; i<len; i++) {
-      var item = obj['data'][i];
-      temp += '<option value="' + item + '">' + item + '</option>'
+  _renderData: function(dom, obj) {
+    // 生成列表模板
+    var valueArr;
+    if(obj['cookie'] && obj['name'] in obj['cookie']) {
+      valueArr = obj['cookie'][obj['name']];
     }
-    // debugger
+
+    var temp = "";
+    for(var i=0; i<obj['data'].length; i++) {
+      var item = obj['data'][i];
+      // 普通item
+      var itemTemp = '<option value="' + item + '">' + item + '</option>';
+      
+      if(valueArr) {
+        for(var j=0; j<valueArr.length; j++) {
+          // 已选中item
+          if(valueArr[j] === item) {
+            itemTemp = '<option value="' + item + '" selected="selected">' + item + '</option>';
+          }
+        }
+      }
+
+      temp += itemTemp;
+    }
+
     dom.innerHTML = '<div class="form-group col-md-4"><label class="control-label col-md-3">' + obj['title'] + '</label><div class="col-md-9"><select class="form-control" id="' + obj['id'] + '" name="' + obj['name'] + '" multiple>' + temp + '</select></div>';
 
     $('#'+obj['id']).select2({
       placeholder: 'please select',
       allowClear: true
-    }).on('select2-open', function(){
-      $(this).data('select2').results.addClass('overflow-hidden').perfectScrollbar();
+    }).on('select2:open', function(){
+      // $(this).data('select2').results.addClass('overflow-hidden').perfectScrollbar();
     });
+  },
+  
+  /**
+   * public function 设置cookie
+   */
+  setCookie: function(cookieKey, cookieStr, day) {
+    $.cookie(cookieKey, cookieStr, { expires: day })
   },
 
   /**
-   * 初始化组件
+   * public function 获取cookie
+   */
+  getCookie: function(cookieKey) {
+    var cookieStr = window.decodeURIComponent($.cookie(cookieKey));
+    var cookiesObj = {};
+    if(cookieStr) {
+      var arr = cookieStr.split('&');
+      // console.log(arr);
+      var checkKey;
+  
+      for(var j=0; j<arr.length; j++) {
+          var key = arr[j].split('=')[0];
+          var value = arr[j].split('=')[1];
+          var _arr = [];
+
+          if(checkKey !== key) {
+            checkKey = key;
+            _arr.push(value);
+            cookiesObj[checkKey] = _arr;
+          } else {
+            if(cookiesObj[key] instanceof Array) {
+              _arr = cookiesObj[key];
+            }
+            _arr.push(value);
+            cookiesObj[key] = _arr;
+          }
+      }
+      // console.log(cookiesObj);
+    }
+    return cookiesObj;
+  },
+
+  /**
+   * public function 初始化组件
    * @author Cc
-   * @date 2016/12/14
    */
   init: function() {
     var that = this;
@@ -44,12 +104,14 @@ var Selector = {
       var title = this.getAttribute('data-title');
       var name = this.getAttribute('data-name');
       var url = this.getAttribute('data-url');
+      var cookieKey = this.getAttribute('data-cookie');
       var selectorDefaultData = ['Data Loading...'];
-      that.renderData(this, {
+      that._renderData(this, {
         'id': id,
         'title': title,
         'name': name,
-        'data': selectorDefaultData
+        'data': selectorDefaultData,
+        'cookie': that.getCookie(cookieKey)
       });
 
       var _this = this;
@@ -61,11 +123,12 @@ var Selector = {
         success: function(res) {
           console.log('%csuccess', 'background: green');
           // debugger
-          that.renderData(_this, {
+          that._renderData(_this, {
             'id': id,
             'title': title,
             'name': name,
-            'data': res.data
+            'data': res.data,
+            'cookie': that.getCookie(cookieKey)
           });
         },
         error: function() {
@@ -85,3 +148,16 @@ var Selector = {
 }
 
 Selector.init();
+
+/**
+ * 绑定提交按钮点击事件
+ */
+$('#submit').bind('click', function(e) {
+  e.stopPropagation();
+  e.preventDefault();
+  var $form = $(this).parents('form');
+  var cookieKey = $form.attr('data-cookie');
+  var cookieStr = $form.serialize();
+  cookieStr ? Selector.setCookie(cookieKey, cookieStr, 7) : alert('请至少选择一项');
+  alert("已点击查询" + cookieStr);
+});
