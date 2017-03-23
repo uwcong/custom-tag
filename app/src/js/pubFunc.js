5193cd24a348eb4ce7d539830063043ebe6a0db2
Cc <uwcong@qq.com>
2017-03-23 09:59:11 +0800

    multiKeys造成加载有误

--- app/src/js/pubFunc.js
+++ app/src/js/pubFunc.js
@@ -39,13 +39,20 @@ var PubFunc = {
                         break;
                     }
                 }
-
                 // key不重复，cookiesObj[key]可能是数组或字符
                 if (checkKey !== key) {
                     checkKey = key;
                     cookiesObj[key] = value;
                     // 若该key是多选，cookiesObj[key]一定要转成数组
                     if (isMulti) cookiesObj[key] = [value];
+                }
+                /**
+                 * key重复，但无multiKeys属性
+                 * 这是为了更新这一版之后，在不清除cookies的情况也能正常显示做的特殊处理，所以 多选的已选项 会与 cookies中的数据不一致，但关系不大。
+                 * 之后的操作就会重写cookies，重写后“key重复”与“multiKeys属性”必定是同时出现的，也就不会走这个步骤了。
+                 */
+                else if (multiKeyArr.length === 0) {
+                    cookiesObj[key] = [value];
                 }
                 // key重复，cookiesObj[key]肯定为数组
                 else {
